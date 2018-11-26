b95d21b6a2b84d479345fe4934e94c342f25b6aa
noel.diaz <noelalbertodg86@gmail.com>
2018-11-26 09:44:27 -0500

    create bot exception manager

---
+++ CustomizeException/BotException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CustomizeException
+{
+    public class BotNotImplementedCommandException : Exception
+    {
+        public BotNotImplementedCommandException() :
+            base()
+        {
+          
+        }
+
+        public BotNotImplementedCommandException(string message)
+            : base(message)
+        {
+            //persist the error log in the pc event viewer
+            EventLog.WriteEntry("BOT", message, EventLogEntryType.Error);
+        }
+
+        public BotNotImplementedCommandException(string message, Exception inner)
+            : base(message, inner)
+        {
+            //persist the error log in the pc event viewer
+            EventLog.WriteEntry("BOT", message, EventLogEntryType.Error);
+        }
+    }
+
+
+
+}
